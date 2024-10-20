d27e24ed90470b5484e0341cf89a469a08fbcb0f
Subham2K2 <90100150+Subham2K2@users.noreply.github.com>
2024-10-20 22:57:25 +0530

    modified Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,33 +17,6 @@ function Navbar() {
 
     return (
         <div>
-            {/* <nav className="navbar navbar-expand-lg navbar-light bg-grey fixed-top $zindex-fixed" style={{ backdropFilter: "blur(10px)" }}>
-                <div className="container">
-                    <Link className="navbar-brand" to="#">Subham....</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded={isOpen ? "true" : "false"} aria-label="Toggle navigation" onClick={handleToggle}>
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item">
-                                <Link className="nav-link active" to="/"> <FontAwesomeIcon icon={faHome} /> Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/aboutme"> <FontAwesomeIcon icon={faUser} /> Aboutme</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/"> <FontAwesomeIcon icon={faFileAlt} /> resume</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/contact"> <FontAwesomeIcon icon={faEnvelope} /> contact</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/projects"> <FontAwesomeIcon icon={faEnvelope} /> projects</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav> */}
             <nav className="navbar navbar-expand-lg navbar-light bg-grey fixed-top" style={{ backdropFilter: "blur(10px)" }}>
                 <div className="container">
                     <Link className="navbar-brand" to="#">Subham....</Link>
@@ -66,7 +39,7 @@ function Navbar() {
                                 <Link className="nav-link" to="/aboutme"> <FontAwesomeIcon icon={faUser} /> Aboutme</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="https://drive.google.com/file/d/1b4XQyVp28z-fHfGz7utcSvqOUwt3uGSv/view?usp=sharing" target="_blank" download="resume" rel="noopener noreferrer">
+                                <a className="nav-link" href="https://drive.google.com/file/d/1Nl04TcOmKjHGyLK7tT5P6G3ZPJR92ko6/view?usp=sharing" target="_blank" download="resume" rel="noopener noreferrer">
                                     <FontAwesomeIcon icon={faFileAlt} /> Resume
                                 </a>
                             </li>
